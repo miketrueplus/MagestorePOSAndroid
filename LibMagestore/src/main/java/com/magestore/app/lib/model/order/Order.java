d7fda6b4e283dc8ab2d90d9992c5ae7e6bff4cf2
Johan <dong.le@trueplus.vn>
2017-01-12 09:20:29 +0700

    khởi tạo model

---
+++ LibMagestore/src/main/java/com/magestore/app/lib/model/order/Order.java
@@ -0,0 +1,13 @@
+package com.magestore.app.lib.model.order;
+
+import com.magestore.app.lib.model.Model;
+
+/**
+ * Created by Johan on 1/12/17.
+ * Magestore
+ * donh.le@trueplus.vn
+ */
+
+public interface Order extends Model{
+
+}
