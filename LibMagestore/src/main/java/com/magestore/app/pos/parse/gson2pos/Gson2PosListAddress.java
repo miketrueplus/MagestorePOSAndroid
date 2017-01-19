25cbcd5c73ae48abe6137f87b430184e1928f579
miketrueplus <mike@trueplus.vn>
2017-01-19 19:28:25 +0700

    Bổ sung AbstractSimpleList từ listview

---
+++ LibMagestore/src/main/java/com/magestore/app/pos/parse/gson2pos/Gson2PosListAddress.java
@@ -0,0 +1,14 @@
+package com.magestore.app.pos.parse.gson2pos;
+
+import com.magestore.app.pos.model.customer.PosCustomer;
+import com.magestore.app.pos.model.customer.PosCustomerAddress;
+
+/**
+ * Parse json customer address sang
+ * Created by Mike on 1/19/2017.
+ * Magestore
+ * mike@trueplus.vn
+ */
+
+public class Gson2PosListAddress extends Gson2PosListParseModel<PosCustomerAddress> {
+}
