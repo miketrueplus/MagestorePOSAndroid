e596e7c211a22980a6aed25b3ef065818a002963
miketrueplus <mike@trueplus.vn>
2017-01-19 19:33:31 +0700

    Bổ sung AbstractSimpleList từ listview

---
+++ LibMagestore/src/main/java/com/magestore/app/pos/parse/gson2pos/Gson2PostListComplain.java
@@ -0,0 +1,12 @@
+package com.magestore.app.pos.parse.gson2pos;
+
+import com.magestore.app.pos.model.customer.PosComplain;
+
+/**
+ * Created by Mike on 1/19/2017.
+ * Magestore
+ * mike@trueplus.vn
+ */
+
+public class Gson2PostListComplain extends Gson2PosListParseModel<PosComplain> {
+}
