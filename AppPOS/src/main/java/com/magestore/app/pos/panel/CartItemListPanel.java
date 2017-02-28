158edad28541c3266dbd76fca4c6dbb0d2ac8b91
Johan <dong.le@trueplus.vn>
2017-02-28 14:06:19 +0700

    sửa swipe delete item card

--- AppPOS/src/main/java/com/magestore/app/pos/panel/CartItemListPanel.java
+++ AppPOS/src/main/java/com/magestore/app/pos/panel/CartItemListPanel.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.daimajia.swipe.SwipeLayout;
 import com.magestore.app.lib.model.checkout.Checkout;
@@ -74,7 +75,7 @@ public class CartItemListPanel extends AbstractListPanel<CartItem> {
 
         // Hiển thị ảnh
         ImageView imageView = (ImageView) view.findViewById(R.id.sales_order_image);
-        if(item.getProduct() != null) {
+        if (item.getProduct() != null) {
             Bitmap bmp = item.getProduct().getBitmap();
             if (bmp != null && !bmp.isRecycled() && imageView != null)
                 imageView.setImageBitmap(bmp);
@@ -85,26 +86,7 @@ public class CartItemListPanel extends AbstractListPanel<CartItem> {
         swipeLayout.addDrag(SwipeLayout.DragEdge.Left, swipeLayout.findViewById(R.id.sales_order_swipe_delete_layout));
 
         // các button trên swipe
-        Button mIncButton = (Button) swipeLayout.findViewById(R.id.sales_order_swipe_inc_quantity);
-        Button mDesButton = (Button) swipeLayout.findViewById(R.id.sales_order_swipe_des_quantity);
-        ImageButton mDelButton = (ImageButton) swipeLayout.findViewById(R.id.sales_order_swipe_del_button);
-
-        // Xử lý sự kiện ấn nút tăng trên swipe
-        mIncButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                getOrderItemListController().addProduct(item.getProduct(), 1);
-                binding.salesOrderSwipeTextview.setText(Integer.toString(item.getQuantity()));
-            }
-        });
-
-        // Xử lý sự kiện ấn nút giảm trên swipe
-        mDesButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (item.getQuantity() <= 1) return;
-                getOrderItemListController().substructProduct(item.getProduct(), 1);
-                binding.salesOrderSwipeTextview.setText(Integer.toString(item.getQuantity()));
-            }
-        });
+        RelativeLayout mDelButton = (RelativeLayout) swipeLayout.findViewById(R.id.sales_order_swipe_del_button);
 
         // Xử lý sự kiện xóa trên swipe
         mDelButton.setOnClickListener(new View.OnClickListener() {
