7989856c20bbbfe3bfdb9c1408efe2ed268d0d79
Johan <dong.le@trueplus.vn>
2017-03-03 08:40:31 +0700

    cập nhật add payment

--- AppPOS/src/main/java/com/magestore/app/pos/panel/CheckoutAddPaymentPanel.java
+++ AppPOS/src/main/java/com/magestore/app/pos/panel/CheckoutAddPaymentPanel.java
@@ -44,8 +44,9 @@ public class CheckoutAddPaymentPanel extends AbstractListPanel<CheckoutPayment> 
 
         // Chuẩn bị list danh sách comment
         // TODO: Mike: tạm khóa đoạn này lại do conflict với recycleview đã có, Phg án là dùng recycle view đang dùng, có colspan = 4
-//        mRecycleView = (RecyclerView) findViewById(R.id.checkout_add_payment_list);
-//        mRecycleView.setLayoutManager(new GridLayoutManager(this.getContext(), 4));
+        mRecycleView = (RecyclerView) findViewById(R.id.checkout_add_payment_list);
+        mRecycleView.setLayoutManager(new GridLayoutManager(this.getContext(), 4));
+        mRecycleView.setAdapter(new AbstractListPanel<CheckoutPayment>.ListRecyclerViewAdapter());
     }
 
     @Override
