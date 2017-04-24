cb91cd68ac260c29ee33a0181a669046c704cb7f
Johan <dong.le@trueplus.vn>
2017-04-24 16:01:27 +0700

    sửa disable config

--- LibMagestore/src/main/java/com/magestore/app/pos/api/m2/config/POSConfigDataAccess.java
+++ LibMagestore/src/main/java/com/magestore/app/pos/api/m2/config/POSConfigDataAccess.java
@@ -108,7 +108,7 @@ public class POSConfigDataAccess extends POSAbstractDataAccess implements Config
 //            statement.setEnableCache("POSConfigDataAccess.getConfig");
 //            statement.getCacheConnection().setReloadCacheLater(true);
 //            statement.getCacheConnection().setForceOutOfDate(true);
-            
+
 //            statement.getCacheConnection().deleteCache();
 
             // Xây dựng tham số
