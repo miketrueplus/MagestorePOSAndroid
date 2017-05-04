e0bb75c877a52540f7b2fb238d28594ca5f26544
Johan <dong.le@trueplus.vn>
2017-05-04 07:59:01 +0700

    sửa lỗi reward point

--- AppPOS/src/main/java/com/magestore/app/pos/panel/PluginRewardPointPanel.java
+++ AppPOS/src/main/java/com/magestore/app/pos/panel/PluginRewardPointPanel.java
@@ -87,6 +87,16 @@ public class PluginRewardPointPanel extends AbstractDetailPanel<RewardPoint> {
             public void onClick(View view) {
                 String quote_id = mCheckoutListController.getSelectedItem().getQuoteId();
                 if (mRewardPoint != null) {
+                    String point = reward_point_value.getText().toString().trim();
+                    if (!StringUtil.isNullOrEmpty(point)) {
+                        int point_value;
+                        try {
+                            point_value = Integer.parseInt(point);
+                        } catch (Exception e) {
+                            point_value = 0;
+                        }
+                        mRewardPoint.setAmount(point_value);
+                    }
                     mRewardPoint.setQuoteId(quote_id);
                     mCheckoutListController.doInputApplyRewardPoint(mRewardPoint);
                 } else {
